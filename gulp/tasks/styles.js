ee74564ea6b8c105ccc249f23933ce7ab2719ed2
mozhnyy <48670931+mozhnyy@users.noreply.github.com>
2020-10-12 19:11:08 +0400

    Добавил изначальный проект для последующей работы

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,57 @@
+const plumber = require("gulp-plumber"),
+	scss = require("gulp-sass"),
+	autoprefixer = require("gulp-autoprefixer"),
+	csso = require("gulp-csso"),
+	csscomb = require("gulp-csscomb"),
+	sourcemaps = require("gulp-sourcemaps"),
+	rename = require("gulp-rename"),
+	qcmq = require("gulp-group-css-media-queries"),
+	stylesPATH = {
+		input: "./dev/static/styles/",
+		output: "./build/static/css/",
+	};
+
+module.exports = function () {
+	$.gulp.task("styles:dev", () => {
+		return $.gulp
+			.src(stylesPATH.input + "styles.scss")
+			.pipe(plumber())
+			.pipe(sourcemaps.init())
+			.pipe(scss())
+			.pipe(
+				autoprefixer({
+					overrideBrowserslist: ["last 3 versions"],
+				})
+			)
+			.pipe(sourcemaps.write())
+			.pipe(rename("styles.css"))
+			.pipe($.gulp.dest(stylesPATH.output))
+			.on("end", $.browserSync.reload);
+	});
+	$.gulp.task("styles:build", () => {
+		return $.gulp
+			.src(stylesPATH.input + "styles.scss")
+			.pipe(scss())
+			.pipe(
+				autoprefixer({
+					overrideBrowserslist: ["last 3 versions"],
+				})
+			)
+			.pipe(autoprefixer())
+			.pipe(qcmq())
+			.pipe(csscomb())
+			.pipe(rename("styles.css"))
+			.pipe($.gulp.dest(stylesPATH.output));
+	});
+	$.gulp.task("styles:build-min", () => {
+		return $.gulp
+			.src(stylesPATH.input + "styles.scss")
+			.pipe(scss())
+			.pipe(autoprefixer())
+			.pipe(qcmq())
+			.pipe(csscomb())
+			.pipe(csso())
+			.pipe(rename("styles.css"))
+			.pipe($.gulp.dest(stylesPATH.output));
+	});
+};
